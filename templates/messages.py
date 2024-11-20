1ee0d1475c8df6a9f78a4f9cc91c592e2cfcc6d1
Kent Clark <123401423+imnotyoursuperman@users.noreply.github.com>
2024-11-20 18:23:20 +0530

    Create messages.py

---
+++ templates/messages.py
@@ -0,0 +1,5 @@
+WELCOME_MESSAGE = "Welcome to the MMO Telegram Bot, {name}! Start creating your character now!"
+PLAYER_INFO_TEMPLATE = (
+    "Name: {name}\nRace: {race}\nClass: {class}\n"
+    "Stats: {stats}\nNickname: {nickname}\nLevel: {level}"
+)
