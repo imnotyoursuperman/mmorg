d0db21548673b8cbe5b9e0dc7457916e4868c983
Kent Clark <123401423+imnotyoursuperman@users.noreply.github.com>
2024-11-20 18:22:40 +0530

    Update __init__.py

--- templates/__init__.py
+++ templates/__init__.py
@@ -1,1 +1,5 @@
+# Initializes the templates package
+from .keyboards import get_customization_keyboard
+from .messages import WELCOME_MESSAGE, PLAYER_INFO_TEMPLATE
 
+__all__ = ["get_customization_keyboard", "WELCOME_MESSAGE", "PLAYER_INFO_TEMPLATE"]
